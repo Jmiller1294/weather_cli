98e88a3c6f3f7bf00f1629dc83b04163da3612f0
Justin Miller <jayman1294@gmail.com>
2019-12-03 02:04:41 +0000

    added to cli class

--- lib/weather_cli/cli.rb
+++ lib/weather_cli/cli.rb
@@ -16,46 +16,44 @@ class WeatherCli::CLI
     
     #gets input from the user
     puts "Type in a city to get its 6 day forecast:"
-    puts ""
-    name = gets
+    
+    @name = gets
     
     # Uses the API class method get_woeid and passes the users input as an arguement
-    WeatherCli::API.get_woeid(name)
-  
+    WeatherCli::API.get_woeid(@name)
+   
+    puts ""
     print_weather
-    
     puts ""
     puts "type in 1-6 to get more information on that day "
     input = gets.strip
 
     day = WeatherCli::Forecast.find(input.to_i)
     
-    
-
     print_weather_day(day)
    
-   #Ask the user if the would like to start the program over again
-   puts "Would you like to see the weather for another day? "
+    #Ask the user if the would like to start the program over again
+    puts "Would you like to see the weather for another day? "
     input = gets.strip.downcase
-    if input == 'y' || input == "yes"
-      WeatherCli::Forecast.clear_all
-      start
-    elsif input == "n" || input == "no"
-      puts ""
-      puts  "See you next time for more weather updates, Goodbye!"
-      exit
-    else
-      puts ""
-      puts "Invalid answer. Enter Yes or No."
-      WeatherCli::Forecast.clear_all
-      start
-    end
+      if input == 'y' || input == "yes"
+        WeatherCli::Forecast.clear_all
+        start
+      elsif input == "n" || input == "no"
+        puts ""
+        puts  "See you next time for more weather updates, Goodbye!"
+        exit
+      else
+        puts ""
+        puts "Invalid answer. Enter Yes or No."
+        WeatherCli::Forecast.clear_all
+        start
+      end
   end
   
   
   def print_weather
     WeatherCli::Forecast.all.each.with_index(0) do |weather, index|
-      puts "#{index + 1}. The weather for this city on #{weather.date}."
+      puts "#{index + 1}. The weather for #{@name.upcase} on #{weather.date}."
     end
   end
   
@@ -75,23 +73,17 @@ class WeatherCli::CLI
   #     puts "-----------------------------------------------------"
   #   end
   
-  
-  
-  
-  
-  
   def print_weather_day(day)
-     
-      #Prints the variables from the Forecast class
-      puts ""
-      puts "The weather for this city on #{day.date} is: "
-      puts "-----------------------------------------------------"
-      puts "Weather Type:           #{day.weather_state_name}"       
-      puts "Current Temperature:    #{((day.current_temp*9/5) + 32).round()} °F"
-      puts "Maximun Temperature:    #{((day.max_temp * 9/5) + 32).round()} °F"
-      puts "Minimum Temperature:    #{((day.min_temp * 9/5) + 32).round()} °F"
-      puts "Wind Speed:             #{day.wind_speed.round(1)} mph"
-      puts "Humidity:               #{day.humidity}%"
-      puts "-----------------------------------------------------"
-    end
+     #Prints the variables from the Forecast class
+    puts ""
+    puts "The weather for #{@name.upcase} on #{day.date} is: "
+    puts "-----------------------------------------------------"
+    puts "Weather Type:           #{day.weather_state_name}"       
+    puts "Current Temperature:    #{((day.current_temp*9/5) + 32).round()} °F"
+    puts "Maximun Temperature:    #{((day.max_temp * 9/5) + 32).round()} °F"
+    puts "Minimum Temperature:    #{((day.min_temp * 9/5) + 32).round()} °F"
+    puts "Wind Speed:             #{day.wind_speed.round(1)} mph"
+    puts "Humidity:               #{day.humidity}%"
+    puts "-----------------------------------------------------"
+  end
 end
