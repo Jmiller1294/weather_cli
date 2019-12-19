8f51e53af6a884ee07ae00472e032740135eaf14
Justin Miller <jayman1294@gmail.com>
2019-12-19 19:11:25 +0000

    made changes

--- lib/weather_cli/cli.rb
+++ lib/weather_cli/cli.rb
@@ -1,12 +1,44 @@
 class WeatherCli::CLI
   
-  
+  # @@sample_data = [
+  #   [-7.1899999999999995, "2019-12-19",54,-4.92,-10.965,"Boston","Clear",11.205149853821682],
+  #   [-3.9000000000000004,"2019-12-20",60,-1.7,-8.04,"Boston","Clear",7.707110488756331],
+  #   [-1.9,"2019-12-21",66,0.02999999999999997,-6.88,"Boston","Heavy Cloud",2.4374862894641955],
+# #<WeatherCli::Forecast:0x00000000027004c0
+#   @current_temp=3.23,
+#   @date="2019-12-22",
+#   @humidity=67,
+#   @max_temp=4.615,
+#   @min_temp=-3.25,
+#   @name="Boston",
+#   @weather_state_name="Light Cloud",
+#   @wind_speed=5.698280536599593>,
+# #<WeatherCli::Forecast:0x0000000002700498
+#   @current_temp=5.52,
+#   @date="2019-12-23",
+#   @humidity=70,
+#   @max_temp=5.824999999999999,
+#   @min_temp=-0.5750000000000001,
+#   @name="Boston",
+#   @weather_state_name="Clear",
+#   @wind_speed=5.5608009339192455>,
+# #<WeatherCli::Forecast:0x0000000002700448
+#   @current_temp=3.08,
+#   @date="2019-12-24",
+#   @humidity=83,
+#   @max_temp=5.62,
+#   @min_temp=-0.09000000000000001,
+#   @name="Boston",
+#   @weather_state_name="Heavy Cloud",
+#   @wind_speed=5.303282828282828>]
   
   #method used to start the program
   def call 
+    #@@sample_data.each {|arr|  WeatherCli::Forecast.new(arr[5], arr[1] ,arr[6], arr[0], arr[3], arr[4], arr[7], arr[2])}
    puts "---------------------------------------"
    puts " Hello, Welcome to the City Weather App "
    puts "---------------------------------------"
+   #binding.pry
    start
   end
   
@@ -16,18 +48,19 @@ class WeatherCli::CLI
     while input !="exit"
     puts "Type in a city to get its 6 day forecast:"
     input = gets.strip
-    WeatherCli::Forecast.find_or_create_by_city_name(input)
-    check_response
-    check_array
+    
     if input  == "exit"
-        puts "Goodbye!"
+        puts "Goodbye"
       else
+        forecast_array = WeatherCli::Forecast.find_or_create_by_city_name(input)
+        check_response
+        print_weather_list(forecast_array)
+        more_info(forecast_array)
         puts "Would you like to search for another city? "
         input = gets.strip.downcase
         if input == "exit"
           puts "Goodbye!"
-        else
-          if input == 'y' || input == "yes"
+        elsif input == 'y' || input == "yes"
             puts "Ok" 
           elsif input == "n" || input == "no"
             puts ""
@@ -40,7 +73,7 @@ class WeatherCli::CLI
         end
       end
     end
-  end
+  
   
   
   def check_response
@@ -50,23 +83,16 @@ class WeatherCli::CLI
     end
   end
 
-  def check_array
-    if WeatherCli::Forecast.array.empty?
-      puts ""
-      print_weather
-      more_info
-      puts ""
-    else
-      puts ""
-      print_weather_array
-      more_info_from_array
-      puts ""
+  def print_weather_list(forecast_array)
+    forecast_array.each.with_index(0) do |weather, index|
+      puts "#{index + 1}. #{weather.date}."
     end
   end
   
   
   
-  def more_info
+  
+  def more_info(forecast_array)
     puts ""
     puts "Type in 1-6 to get a detailed report for one these dates"
     input = gets.strip.downcase
@@ -74,7 +100,10 @@ class WeatherCli::CLI
       exit
     else
       if input.to_i >= 1 && input.to_i <= 6
-        day = WeatherCli::Forecast.find(input.to_i)
+        #binding.pry
+        day = forecast_array[input.to_i-1]
+        # pass in WHOLE object FROM LIST
+        # day = WeatherCli::Forecast.find_object(OBJECT)
         print_weather_day(day)
       else
         puts "Wrong number entered, please enter number 1-6"
@@ -100,11 +129,6 @@ class WeatherCli::CLI
   end
   
 
-  def print_weather
-    WeatherCli::Forecast.all.last(6).each.with_index(0) do |weather, index|
-      puts "#{index + 1}. #{weather.date}."
-    end
-  end
   
   
   def print_weather_array
--- lib/weather_cli/forecast.rb
+++ lib/weather_cli/forecast.rb
@@ -4,9 +4,9 @@ class WeatherCli::Forecast
   
   #creates setter and getter methods
   attr_accessor :id, :name , :date, :weather_state_name ,:current_temp , :min_temp, :max_temp,:wind_speed, :humidity 
-  
   @@all = []
-  @@array = []
+ 
+  
 
   def initialize(name, date ,weather_state_name, current_temp, max_temp, min_temp, wind_speed, humidity)
     @name = name
@@ -20,7 +20,7 @@ class WeatherCli::Forecast
     @@all << self
   end
   
-
+  
   def self.all
     @@all
   end
@@ -29,16 +29,14 @@ class WeatherCli::Forecast
     @@array
   end 
   
-  def self.find(id)
-    self.all.last(6)[id-1]
+  def self.find_object(obj)
+    binding.pry
+    #find the obj from @@all 
+    [id-1]
   end
   
   def self.find_from_array(index)
     self.array[index -1]
-  end
-  
-  def self.clear_all
-    @@all.clear
   end
 
   def self.find_by_city_name(name)
@@ -51,11 +49,12 @@ class WeatherCli::Forecast
   
   
   def self.find_or_create_by_city_name(user_input)
-    @@array = self.find_by_city_name(user_input)
-    if @@array.empty?
-      WeatherCli::API.get_woeid(user_input)
+    array = self.find_by_city_name(user_input)
+    if array.empty?
+    WeatherCli::API.get_woeid(user_input)
+    self.all.last(6)
     else
-      @@array
+      array
     end
   end
   
